d48bdd9d4d2feb88c21e3959bf8a3a89e1ca9f07
RzayevGara <qara109@gmail.com>
2022-10-12 15:03:42 +0400

    product swiper done

--- src/components/product/product-swiper/SwiperProduct.vue
+++ src/components/product/product-swiper/SwiperProduct.vue
@@ -18,19 +18,19 @@
             </ul>
         </div>
         <div class="product-swiper_items">
-            <div class="product-swiper-items-leftArrow">
+            <div class="product-swiper-items-leftArrow" @click="leftSlide">
                 <ArrowIcon/>
             </div>
             <div class="product-swiper-items_slider">
-                <ul>
-                    <li v-for="(el, index) in item.assets" :key="el.id" v-show="index>0">
-                        <div class="product-swiper-items_image">
+                <ul ref="smallSlider">
+                    <li v-for="(el, index) in item.assets" :key="el.id" v-show="index>0" @click="imageClick(el.id)">
+                        <div :class="el.id===activeImageID?  'product-swiper-items_image active-image': 'product-swiper-items_image'">
                             <img :src="el.url" alt="image"/>
                         </div>
                     </li>
                 </ul>
             </div>
-            <div class="product-swiper-items-rightArrow">
+            <div class="product-swiper-items-rightArrow" @click="rightSlide">
                 <ArrowIcon/>
             </div>
         </div>
@@ -39,30 +39,95 @@
 
 <script setup>
     import ArrowIcon from '../../../assets/image/svg/arrow-up.svg'
-    import {ref} from 'vue'
+    import {ref, watch} from 'vue'
+    import {useStore} from 'vuex'
+
+    const store = useStore()
     const slider = ref(null)
-    const count = ref(0)
+    const countSlider = ref(0)
+    const smallSlider = ref(null)
+    const countSmallSlider = ref(0)
     const props = defineProps({item: Object})
+    const activeImageID  = ref(store.state.product.activeImageID)
+
+    watch(store.state.product, (to)=>{
+        activeImageID.value = to.activeImageID
+    })
 
     function rightClick(){
-        if(count.value > -(props.item.assets.length-2)*100){
-            count.value -= 100
-            slider.value.style.transform = `translateX(${count.value}%)`
-        }else{
-            count.value = 0
-            slider.value.style.transform = `translateX(${count.value}%)`
+        if(countSlider.value > -(props.item.assets.length-2)*100){
+            countSlider.value -= 100
+            slider.value.style.transform = `translateX(${countSlider.value}%)`
+            store.commit("setActiveImageID", props.item.assets[((countSlider.value/100)*-1)+1].id)
             
+            props.item.assets.map((item, index)=>{
+                if(item.id===activeImageID.value){
+                    let count = Math.floor((index)/3)
+                    smallSlider.value.style.transform = `translate3d(calc(${count * -25}% + ${count * -15}px), 0px, 0px)`
+                }
+            })
+        }else{
+            countSlider.value = 0
+            slider.value.style.transform = `translateX(${countSlider.value}%)`
+            store.commit("setActiveImageID", props.item.assets[1].id)
+            let count = 0
+            smallSlider.value.style.transform = `translate3d(calc(${count * -25}% + ${count * -15}px), 0px, 0px)`
         }
+
     }
     function leftClick(){
-        if(count.value < 0){
-            count.value += 100
-            slider.value.style.transform = `translateX(${count.value}%)`
+        if(countSlider.value < 0){
+            countSlider.value += 100
+            slider.value.style.transform = `translateX(${countSlider.value}%)`
+            store.commit("setActiveImageID", props.item.assets[((countSlider.value/100)*-1)+1].id)
+            props.item.assets.map((item, index)=>{
+                if(item.id===activeImageID.value){
+                    let count = Math.floor((index-2)/3)
+                    smallSlider.value.style.transform = `translate3d(calc(${count * -25}% + ${count * -15}px), 0px, 0px)`
+                }
+            })
         }
         else{
-            count.value = -(props.item.assets.length-2)*100
-            slider.value.style.transform = `translateX(${count.value}%)`
+            countSlider.value = -(props.item.assets.length-2)*100
+            slider.value.style.transform = `translateX(${countSlider.value}%)`
+            store.commit("setActiveImageID", props.item.assets[props.item.assets.length-1].id)
             
+            let count = Math.floor((props.item.assets.length-2)/3)
+            smallSlider.value.style.transform = `translate3d(calc(${count * -25}% + ${count * -15}px), 0px, 0px)`
+        }
+    }
+
+    function imageClick(id){
+        props.item.assets.map((el, index)=>{
+            if(el.id===id){
+                countSlider.value = (index-1)*-100
+                slider.value.style.transform = `translateX(${countSlider.value}%)`
+                store.commit("setActiveImageID", id)
+            }
+        })
+    }
+
+    function leftSlide(){
+        let count = Math.ceil((props.item.assets.length-1)/3)-1
+        if( countSmallSlider.value===0){
+            countSmallSlider.value=count
+            smallSlider.value.style.transform = `translate3d(calc(${countSmallSlider.value * -25}% + ${countSmallSlider.value * -15}px), 0px, 0px)`
+        }
+        else if (count>=countSmallSlider.value){
+            countSmallSlider.value-=1
+            smallSlider.value.style.transform = `translate3d(calc(${countSmallSlider.value * -25}% + ${countSmallSlider.value * -15}px), 0px, 0px)`
+        }
+    }
+
+    function rightSlide(){
+        let count = Math.ceil((props.item.assets.length-1)/3)-1
+        if(count>countSmallSlider.value){
+            countSmallSlider.value+=1
+            smallSlider.value.style.transform = `translate3d(calc(${countSmallSlider.value * -25}% + ${countSmallSlider.value * -15}px), 0px, 0px)`
+        }
+        else{
+            countSmallSlider.value=0
+            smallSlider.value.style.transform = `translate3d(calc(${countSmallSlider.value * -25}% + ${countSmallSlider.value * -15}px), 0px, 0px)`
         }
     }
 </script>
--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -7,17 +7,22 @@ const commerce = (typeof import.meta.env.VITE_CHEC_PUBLIC_KEY !== 'undefined')
 export default {
     state:{
         product: null,
+        activeImageID: null
     },
     mutations: {
         setProduct(state, item) {
           state.product = item
         },
+        setActiveImageID(state, item) {
+          state.activeImageID = item
+        }
     },
     actions: {
         fetchProduct ({commit}, id) {
             commerce.products.retrieve(id, { type: 'permalink' })
             .then((products) => {
                 commit("setProduct", products);
+                commit("setActiveImageID", products.assets[1].id);
             })
             .catch((error) => {
               console.log('There is an error fetching products', error);
@@ -28,5 +33,8 @@ export default {
       getProduct(state){
         return state.product
       },
+      getActiveImageID(state){
+        return state.activeImageID
+      }
     }
 }
